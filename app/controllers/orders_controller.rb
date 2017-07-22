dfcda0ac312e70c0d00ed3a2eaae168246853844
chenxiaoqing <pincha120@gmail.com>
2017-07-22 12:05:33 +0800

    add OrderMailer function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,6 +17,9 @@ class OrdersController < ApplicationController
         product_list.save
       end
 
+      current_cart.clean!
+      OrderMailer.notify_order_placed(@order).deliver!
+
       redirect_to order_path(@order.token)
     else
       render 'carts/checkout'
@@ -43,7 +46,7 @@ class OrdersController < ApplicationController
 
     redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
   end
-  
+
   private
 
   def order_params
